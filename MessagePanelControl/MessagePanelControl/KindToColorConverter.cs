bedc24526c119fe4bb643d738ab92563a7b87d36
Amadeus Wieczorek <git@amadeusw.com>
2014-09-18 13:54:43 -0400

    Styling GUI and adding converters

---
+++ MessagePanelControl/MessagePanelControl/KindToColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AmadeusW.MessagePanelControl
+{
+    [ValueConversion(typeof(MessageKind), typeof(SolidColorBrush))]
+    public class KindToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is MessageKind))
+            {
+                return new SolidColorBrush(Colors.Gainsboro);
+            }
+            switch ((MessageKind)value)
+            {
+                case MessageKind.Info:
+                    return new SolidColorBrush(Colors.DeepSkyBlue);
+                case MessageKind.Fatal:
+                    return new SolidColorBrush(Colors.Firebrick);
+                case MessageKind.Error:
+                    return new SolidColorBrush(Colors.OrangeRed);
+                case MessageKind.Warning:
+                    return new SolidColorBrush(Colors.Goldenrod);
+                case MessageKind.Success:
+                    return new SolidColorBrush(Colors.GreenYellow);
+                default:
+                    return new SolidColorBrush(Colors.Gainsboro);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MessagePanelControl/MessagePanelControl/KindToGlyphConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AmadeusW.MessagePanelControl
+{
+    /// <summary>
+    /// Converts MessageKind to a glyph in Wingdings font
+    /// </summary>
+    [ValueConversion(typeof(MessageKind), typeof(String))]
+    public class KindToGlyphConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is MessageKind))
+            {
+                return " ";
+            }
+            switch ((MessageKind)value)
+            {
+                case MessageKind.Info:
+                    return "w";
+                case MessageKind.Fatal:
+                    return "I";
+                case MessageKind.Error:
+                    return "û";
+                case MessageKind.Warning:
+                    return "Ö";
+                case MessageKind.Success:
+                    return "ü";
+                default:
+                    return " ";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
